4ced5d3637d17a451a9e40946b075094fbe0452d
IamAFK2112 <44500829+IamAFK2112@users.noreply.github.com>
2020-02-04 14:52:21 +0000

    organised screens

---
+++ fdm_expenses_app/lib/screens/wrapper.dart
@@ -0,0 +1,10 @@
+import 'package:fdm_expenses_app/screens/home/home.dart';
+import 'package:flutter/material.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    //return either home or authenticate widget
+    return Home();
+  }
+}

7b7e41b71e23289e9ff7e69ce005337b0d0fce3e
IamAFK2112 <44500829+IamAFK2112@users.noreply.github.com>
2020-02-04 15:49:57 +0000

    created user class which contains relevant information about the user

--- fdm_expenses_app/lib/screens/services/auth.dart
+++ fdm_expenses_app/lib/screens/services/auth.dart
@@ -1,15 +1,21 @@
+import 'package:fdm_expenses_app/models/user.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService {
 
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
+  //create user object from firebase user
+  User _userFromFirebaseUser(FirebaseUser user) {
+    return user != null ? User(uid: user.uid) : null;
+  }
+
   //sign in anonymously
   Future signInAnon() async {
     try {
       AuthResult result = await _auth.signInAnonymously();
       FirebaseUser user = result.user;
-      return user;
+      return _userFromFirebaseUser(user);
     } catch(e) {
       print(e.toString());
       return null;

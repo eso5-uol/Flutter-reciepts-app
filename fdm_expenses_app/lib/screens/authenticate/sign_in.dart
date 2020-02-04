6cde0f9b3fa8417dd11a4fa2b0dded54b27886bc
IamAFK2112 <44500829+IamAFK2112@users.noreply.github.com>
2020-02-04 15:44:17 +0000

    Added anonymous authentication method (for testing purposes)

---
+++ fdm_expenses_app/lib/screens/authenticate/sign_in.dart
@@ -0,0 +1,39 @@
+import 'package:fdm_expenses_app/screens/services/auth.dart';
+import 'package:flutter/material.dart';
+
+class SignIn extends StatefulWidget {
+  @override
+  _SignInState createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+
+  final AuthService _auth = AuthService();
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.brown[100],
+      appBar: AppBar(
+        title: Text("Sign into FDM Expenses"),
+        backgroundColor: Colors.brown[400],
+      ),
+      body: Container(
+        padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
+        child: RaisedButton(
+          child: Text("Sign in anonymously"),
+          onPressed: () async {
+            dynamic result = await _auth.signInAnon();
+            if (result == null) {
+              print("Error signing in");
+            } else {
+              print("Signed in");
+              print(result);
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
--- fdm_expenses_app/lib/screens/wrapper.dart
+++ fdm_expenses_app/lib/screens/wrapper.dart
@@ -1,3 +1,4 @@
+import 'package:fdm_expenses_app/screens/authenticate/authenticate.dart';
 import 'package:fdm_expenses_app/screens/home/home.dart';
 import 'package:flutter/material.dart';
 
@@ -5,6 +6,6 @@ class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     //return either home or authenticate widget
-    return Home();
+    return Authenticate();
   }
 }

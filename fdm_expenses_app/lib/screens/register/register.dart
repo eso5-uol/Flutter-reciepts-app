442ead718d4e88ca70bf292d03031b86c712fca8
Hayden Woods <hpw7@student.le.ac.uk>
2020-02-09 15:34:53 +0000

    Split Register class into _RegisterState and added authentication service and necessary variables

--- fdm_expenses_app/lib/screens/register/register.dart
+++ fdm_expenses_app/lib/screens/register/register.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:fdm_expenses_app/screens/services/auth.dart';
 
-class Register extends StatelessWidget {
+class Register extends StatefulWidget {
+  @override
+  _RegisterState createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+
+  final AuthService _auth = AuthService();
+
+  String _email = "";
+  String _defaultPassword = "default";
+  String error = "";
 
   @override
   Widget build(BuildContext context) {

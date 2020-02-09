28d3e8207756b3adc14641d40581d7292c815b4e
Hayden Woods <hpw7@student.le.ac.uk>
2020-02-09 15:07:00 +0000

    Basic structure for account register screen

---
+++ fdm_expenses_app/lib/screens/register/register.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Register extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.brown[50],
+      appBar: AppBar(
+        backgroundColor: Colors.brown[400],
+        title: Text("FDM Expenses"),
+      ),
+    );
+  }
+}

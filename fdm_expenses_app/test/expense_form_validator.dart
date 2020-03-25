05385f3710580ac53746854ad91b158c66348963
T Sandler <174-ts340@users.noreply.campus.cs.le.ac.uk>
2020-03-25 12:41:31 +0000

    Update expense_form_validator.dart

--- fdm_expenses_app/test/expense_form_validator.dart
+++ fdm_expenses_app/test/expense_form_validator.dart
@@ -58,4 +58,84 @@ void main() {
     expect(result, 'Enter an amount of milage claimed');
   });
 
+  test('Invalid format of flights', () {
+    var result = Validator.dateFormat("£00.000");
+    expect(result, "flights is formatted badly!");
+  });
+
+  test('Empty flights claimed returns error string', () {
+    var result = Validator.dateFormat('');
+    expect(result, 'Enter an amount of flights');
+  });
+
+  test('Invalid format of other travel', () {
+    var result = Validator.dateFormat("£00.000");
+    expect(result, "other travel is formatted badly!");
+  });
+
+  test('Empty other travel claimed returns error string', () {
+    var result = Validator.dateFormat('');
+    expect(result, 'Enter an amount of other travel');
+  });
+
+  test('Invalid format of accomodation', () {
+    var result = Validator.dateFormat("£00.000");
+    expect(result, "accomodation is formatted badly!");
+  });
+
+  test('Empty accomodation claimed returns error string', () {
+    var result = Validator.dateFormat('');
+    expect(result, 'Enter an amount of accomodation');
+  });
+
+  test('Invalid format of subsistence', () {
+    var result = Validator.dateFormat("£00.000");
+    expect(result, "subsistence is formatted badly!");
+  });
+
+  test('Empty subsistence claimed returns error string', () {
+    var result = Validator.dateFormat('');
+    expect(result, 'Enter an amount of subsistence');
+  });
+
+  test('Invalid format of staff entertaining', () {
+    var result = Validator.dateFormat("£00.000");
+    expect(result, "staff entertaining is formatted badly!");
+  });
+
+  test('Empty staff entertaining claimed returns error string', () {
+    var result = Validator.dateFormat('');
+    expect(result, 'Enter an amount of staff entertaining');
+  });
+
+  test('Invalid format of client entertaining', () {
+    var result = Validator.dateFormat("£00.000");
+    expect(result, "client entertaining is formatted badly!");
+  });
+
+  test('Empty client entertaining claimed returns error string', () {
+    var result = Validator.dateFormat('');
+    expect(result, 'Enter an amount of client entertaining');
+  });
+
+  test('Invalid format of other', () {
+    var result = Validator.dateFormat("£00.000");
+    expect(result, "other is formatted badly!");
+  });
+
+  test('Empty other claimed returns error string', () {
+    var result = Validator.dateFormat('');
+    expect(result, 'Enter an amount of other');
+  });
+
+  test('Invalid format of VAT', () {
+    var result = Validator.dateFormat("£00.000");
+    expect(result, "VAT is formatted badly!");
+  });
+
+  test('Empty VAT claimed returns error string', () {
+    var result = Validator.dateFormat('');
+    expect(result, 'Enter an amount of VAT');
+  });
+
 }
